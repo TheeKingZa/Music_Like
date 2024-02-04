073425db9b4e5aaaf00391d2d060b7e4556df127
Pule <proceedingmc@gmail.com>
2024-02-04 17:43:23 +0200

    set up the login process

--- app/app.py
+++ app/app.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template
-# from app import db
+from flask import Flask, render_template, request, url_for, redirect
 
 # flask(app) instance.
 app = Flask(__name__)
+
+username = 'admin'
+password = 'admin'
 
 # Define routes
 @app.route('/')
 # home.page
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
+
+# Logins Conditions
+@app.route('/login', methods=['POST'])
+def login():
+    # Get username and password
+    entered_username = request.form['username']
+    entered_password = request.form['password']
+    
+    # Check if the entered username and password to match values
+    # Hardcopy of Admin
+    if entered_username == username and entered_password == password:
+        # Redirect to home page if successful
+        return redirect(url_for('home'))
+    else:
+        error = "Invalid Username or Password. Please Try Again"
+        return render_template('login.html', error=error)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 @app.route('/contact')
 def contact():
@@ -31,4 +53,3 @@ def recommendations():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
