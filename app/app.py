4878a3a944fb59a3a9426a99108b2a1b98c6dbbf
Pule <proceedingmc@gmail.com>
2024-02-08 14:52:23 +0200

    requirements update

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ with open('data/songs.json', 'r') as file:
 # Load user_data
 def read_user_data():
     try:
-        with open('user_db.json', 'r') as file:
+        with open('.user_db.json', 'r') as file:
             return json.load(file)
     except FileNotFoundError:
         return []
@@ -26,7 +26,7 @@ def read_user_data():
 def add_user_data(user_data):
     users = read_user_data()
     users.append(user_data)
-    with open('user_db.json', 'w') as file:
+    with open('.user_db.json', 'w') as file:
         json.dump(users, file, indent=4)
     
 # Routes
